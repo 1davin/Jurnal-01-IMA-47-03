348292c6e2df73e0ec7a9d0ed012b61ae3bfe32e
1davin <davinahmad53@gmail.com>
2023-09-27 16:05:10 +0700

    Initial Commit

---
+++ Jurnal01.java
@@ -0,0 +1,13 @@
+public class Jurnal01 {
+    public static void main(String[] args) {
+        String Nama, Kelas, NIM;
+        Nama = "Davin Ahmad Alaudin";
+        Kelas = "D3IF-47-03";
+        NIM = "607062300115";
+
+        System.out.println("Nama\t:"+Nama);
+        System.out.println("Kelas\t:"+Kelas);
+        System.out.println("NIM\t:"+NIM);
+    }
+    
+}
